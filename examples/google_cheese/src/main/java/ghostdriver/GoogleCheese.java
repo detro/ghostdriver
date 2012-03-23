ba6179692b9c58718c7538565719b0edaf04f765
Ivan De Marino <ivan.de.marino@gmail.com>
2012-03-23 23:52:32 +0000

    Added (and commented) the code to make the "Google Cheese" example quickly switch to use Firefox.
    This will be useful during demos ;)

--- examples/google_cheese/src/main/java/ghostdriver/GoogleCheese.java
+++ examples/google_cheese/src/main/java/ghostdriver/GoogleCheese.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -13,22 +14,21 @@ import java.net.URL;
 
 public class GoogleCheese {
     public static void main( String[] args ) throws MalformedURLException {
-        // Create a new instance of the html unit driver
-        // Notice that the remainder of the code relies on the interface,
-        // not the implementation.
-
         // Ask for a JavaScript-enabled browser
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setJavascriptEnabled(true);
 
         // Get a handle to the driver. This will throw an exception if a matching driver cannot be located
         WebDriver driver = new RemoteWebDriver(new URL("http://localhost:8080"), capabilities);
+//        WebDriver driver = new FirefoxDriver(capabilities);
 
         // Query the driver to find out more information
         Capabilities actualCapabilities = ((RemoteWebDriver) driver).getCapabilities();
+//        Capabilities actualCapabilities = ((FirefoxDriver) driver).getCapabilities();
 
         // And now use this to visit Google
         driver.get("http://www.google.com");
+        System.out.println("Current URL is: " + driver.getCurrentUrl());
 
         // Find the text input element by its name
         WebElement element = driver.findElement(By.name("q"));
@@ -41,5 +41,6 @@ public class GoogleCheese {
 
         // Check the title of the page
         System.out.println("Page title is: " + driver.getTitle());
+        driver.close();
     }
 }
