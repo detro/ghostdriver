50d48808a8d1e29ff3ee4dd29e93c1a92a676282
Ivan De Marino <ivan.de.marino@gmail.com>
2012-03-23 23:53:09 +0000

    Implemented commands GET and POST "/session/:sessionId/url"

--- request_handlers/session_request_handler.js
+++ request_handlers/session_request_handler.js
@@ -7,9 +7,37 @@ ghostdriver.SessionReqHand = function(session) {
     _session = session,
 
     _handle = function(req, res) {
+        var postObj,
+            url;
+
         _protoParent.handle.call(this, req, res);
 
-        // TODO - Continue from here...
+        if (req.urlParsed.file === "url") {
+            if (req.method === "GET") {
+                // Get the URL at which the Page currently is
+                url = _session.getPage().evaluate(function() { return location.toString(); });
+                res.statusCode = 200;
+                res.writeJSON(_protoParent.buildSuccessResponseBody.call(this, _session.getId(), url));
+                res.close();
+            } else if (req.method === "POST") {
+                // Load the given URL in the Page
+                postObj = JSON.parse(req.post);
+                if (typeof(postObj) === "object" && postObj.url) {
+                    // Open the given URL and, when done, return "HTTP 200 OK"
+                    _session.getPage().open(postObj.url, function(status) {
+                        if (status === "success") {
+                            res.statusCode = 200;
+                            res.closeGracefully();
+                        } else {
+                            throw new ghostdriver.InvalidCommandMethod(req);
+                        }
+                    });
+                } else {
+                    throw new ghostdriver.MissingCommandParameters(req);
+                }
+            }
+            return;
+        }
 
         throw new ghostdriver.InvalidCommandMethod(req);
     };
