bdf8c2a35687d8bfc9adccca017ce61fce3dd2fe
Ivan De Marino <ivan.de.marino@gmail.com>
2012-03-24 23:50:41 +0000

    Decorating the "webpage" object with "evaluateWithParams".

--- session.js
+++ session.js
@@ -28,6 +28,25 @@ ghostdriver.Session = function(desiredCapabilities) {
     _page = require("webpage").create(),
     _id = (++ghostdriver.Session.instanceCounter) + ''; //< must be a string, even if I use progressive integers as unique ID
 
+    // Decorating the "webpage" object
+    _page.evaluateWithParams = function(func) {
+        var args = [].slice.call(arguments, 1),
+            str = 'function() { return (' + func.toString() + ')(',
+            i, ilen, arg;
+
+        for (i = 0, ilen = args.length; i < ilen; ++i) {
+            arg = args[i];
+            if (/object|string/.test(typeof arg)) {
+                str += 'JSON.parse(' + JSON.stringify(JSON.stringify(arg)) + '),';
+            } else {
+                str += arg + ',';
+            }
+        }
+        str = str.replace(/,$/, '); }');
+
+        return this.evaluate(str);
+    };
+
     // public:
     return {
         getCapabilities : function() { return _defaultCapabilities; },
