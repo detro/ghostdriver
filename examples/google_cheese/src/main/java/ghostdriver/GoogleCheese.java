e90e2edf25294074561103cfa663ffc64178f10a
Ivan De Marino <ivan.de.marino@gmail.com>
2012-03-27 15:37:25 +0100

    Made the GoogleCheese test also start and stop PhantomJS, so that timing is more 'fair'. Still blazing fast though.

--- examples/google_cheese/src/main/java/ghostdriver/GoogleCheese.java
+++ examples/google_cheese/src/main/java/ghostdriver/GoogleCheese.java
@@ -7,7 +7,11 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.UnreachableBrowserException;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Date;
@@ -17,8 +21,10 @@ public class GoogleCheese {
     private static final int USE_GHOSTDRIVER = 0;
     private static final int USE_FIREFOX_DRIVER = 1;
     private static final int USE_CHROME_DRIVER = 2;
+    private static final String PHANTOMJS_PATH = "/Users/detro/Workspaces/Qt/phantomjs/bin/phantomjs";   //< CHANGEME
+    private static final String GHOSTDRIVER_PATH = "/Users/detro/Workspaces/Qt/ghostdriver/main.js";     //< CHANGEME
 
-    public static void main( String[] args ) throws MalformedURLException {
+    public static void main( String[] args ) throws MalformedURLException, IOException, InterruptedException {
         int use = USE_GHOSTDRIVER;
 
         if (args.length == 1) {
@@ -36,6 +42,7 @@ public class GoogleCheese {
 
         WebDriver driver = null;
         Capabilities actualCapabilities = null;
+        Process phantomjs = null;
 
         switch (use) {
             case USE_FIREFOX_DRIVER: {
@@ -54,10 +61,21 @@ public class GoogleCheese {
             }
             case USE_GHOSTDRIVER: default: {
                 System.out.println("*** USING GHOSTDRIVER ***");
-                // Get a handle to the driver. This will throw an exception if a matching driver cannot be located
-                driver = new RemoteWebDriver(new URL("http://localhost:8080"), capabilities);
-                // Query the driver to find out more information
-                actualCapabilities = ((RemoteWebDriver) driver).getCapabilities();
+                phantomjs = Runtime.getRuntime().exec(PHANTOMJS_PATH + ' ' + GHOSTDRIVER_PATH);
+
+                BufferedReader phantomOutputReader = new BufferedReader (new InputStreamReader(phantomjs.getInputStream()));
+                while (phantomOutputReader.readLine () == null) { } //< As soon as Ghostdriver outputs something, we can continue
+                System.out.println("PhantomJS started in (ms): " + (new Date().getTime() - start.getTime()));
+                phantomOutputReader.close();
+
+                try {
+                    // Get a handle to the driver. This will throw an exception if a matching driver cannot be located
+                    driver = new RemoteWebDriver(new URL("http://localhost:8080"), capabilities);
+                    // Query the driver to find out more information
+                    actualCapabilities = ((RemoteWebDriver) driver).getCapabilities();
+                } catch (UnreachableBrowserException e) {
+                    phantomjs.destroy();
+                }
                 break;
             }
         }
@@ -85,6 +103,9 @@ public class GoogleCheese {
         // Check the title of the page
         System.out.println("Page title is: " + driver.getTitle());
         driver.close();
+        if (phantomjs != null) {
+            phantomjs.destroy();
+        }
         
         System.out.println("Time elapsed (ms): " + (new Date().getTime() - start.getTime()));
     }
