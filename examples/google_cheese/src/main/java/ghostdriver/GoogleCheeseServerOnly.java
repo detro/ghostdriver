65103b5cf97977f324ed0aa37dad168424c0dc18
Ivan De Marino <ivan.de.marino@gmail.com>
2012-03-30 16:31:03 +0100

    New test: Modified version of "Google Cheese!" to run only using RemoteDriver servers

---
+++ examples/google_cheese/src/main/java/ghostdriver/GoogleCheeseServerOnly.java
@@ -0,0 +1,95 @@
+package ghostdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Date;
+
+
+public class GoogleCheeseServerOnly {
+    public static void main( String[] args ) throws MalformedURLException, IOException, InterruptedException, NumberFormatException {
+        int samplingSize = 1;
+        int port = 8080;
+
+        if (args.length > 0) {
+            samplingSize = Integer.parseInt(args[0]);
+        }
+
+        if (args.length > 1) {
+            port = Integer.parseInt(args[1]);
+        }
+
+        System.out.println("Sampling Size: "+samplingSize);
+        System.out.println("Remote WebDriver Server port: "+port);
+
+        // Let's make some stats
+        long stats = 0;
+
+        // Sum up samples
+        for (int i = 0; i < samplingSize; ++i) {
+            stats += runTest(port);
+        }
+
+        // Normalize
+        stats /= samplingSize;
+
+        System.out.println("");
+        System.out.println("--------------------------------------");
+        System.out.println("*** STATS");
+        System.out.println("*** Total Execution Time over "+samplingSize+" samples: "+stats);
+        System.out.println("--------------------------------------");
+        System.out.println("");
+
+        System.exit(0);
+    }
+    
+    public static long runTest(int port) throws MalformedURLException, IOException, InterruptedException {
+        long totalTime;
+
+        // Start the clock
+        Date start = new Date();
+
+        // Ask for a JavaScript-enabled browser
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setJavascriptEnabled(true);
+
+        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:"+port), capabilities);
+        Capabilities actualCapabilities = ((RemoteWebDriver) driver).getCapabilities();
+
+        // And now use this to visit Google
+        System.out.println("Loading 'http://www.google.com'...");
+        driver.get("http://www.google.com");
+        System.out.println("Loaded. Current URL is: '" + driver.getCurrentUrl() + "'");
+
+        // Find the text input element by its name
+        System.out.println("Finding an Element via [name='q']...");
+        WebElement element = driver.findElement(By.name("q"));
+        System.out.println("Found.");
+
+        // Enter something to search for
+        System.out.println("Sending keys 'Cheese!...'");
+        element.sendKeys("Cheese!");
+        System.out.println("Sent.");
+
+        // Now submit the form. WebDriver will find the form for us from the element
+        System.out.println("Submitting Element...");
+        element.submit();
+        System.out.println("Submitted.");
+
+        // Check the title of the page
+        System.out.println("Page title is: " + driver.getTitle());
+        driver.close();
+
+        totalTime = new Date().getTime() - start.getTime();
+        System.out.println("Time elapsed (ms): " + totalTime);
+
+        return totalTime;
+    }
+}
