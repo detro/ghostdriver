33361d50c223489a5a7c814558bcbc6cf81a93b6
Jim Evans <james.h.evans.jr@gmail.com>
2012-06-29 13:26:07 -0400

    Adding test for hidden element.

--- test/src/test/java/ghostdriver/ElementMethodsTest.java
+++ test/src/test/java/ghostdriver/ElementMethodsTest.java
@@ -12,14 +12,16 @@ import static org.junit.Assert.assertTrue;
 public class ElementMethodsTest extends BaseTest {
     @Test
     public void checkDisplayedOnGoogleSearchBox() {
-        // TODO: Find a sample site that has hidden elements and use it to
-        // verify behavior of displayed and non-displayed elements.
         WebDriver d = getDriver();
 
         d.get("http://www.google.com");
         WebElement el = d.findElement(By.cssSelector("input[name*='q']"));
 
         assertTrue(el.isDisplayed());
+
+        el = d.findElement(By.cssSelector("input[type='hidden']"));
+
+        assertFalse(el.isDisplayed());
     }
 
     @Test
