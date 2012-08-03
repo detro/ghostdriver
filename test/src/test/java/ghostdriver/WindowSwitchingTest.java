15507bc9b531603ff430c848df01153d32715f65
Ivan De Marino <ivan.de.marino@gmail.com>
2012-08-03 19:55:14 +0100

    Sorting out a bit the Window Switching Tests

--- test/src/test/java/ghostdriver/WindowSwitchingTest.java
+++ test/src/test/java/ghostdriver/WindowSwitchingTest.java
@@ -2,6 +2,7 @@ package ghostdriver;
 
 import org.junit.Test;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchFrameException;
 import org.openqa.selenium.WebDriver;
 
 import java.util.ArrayList;
@@ -35,24 +36,17 @@ public class WindowSwitchingTest extends BaseTest {
         assertTrue(!bingWH.equals(googleWH));
         assertTrue(!bingWH.equals(yahooWH));
 
-        // Delete yahoo window and try to access it :should cause an error
-        boolean threw = false;
-        try {
-            d.close();
-            d.getTitle();
-        } catch (Throwable t) {
-            threw = true;
-        }
-        assertTrue(threw);
+        // Close yahoo window
+        d.close();
 
-        // Swtich to google window and notice that it's the only left (bing was closed as it was a child of yahoo)
+        // Switch to google window and notice that only google and bing are left
         d.switchTo().window(googleWH);
         assertEquals(d.getWindowHandles().size(), 2);
         assertTrue(d.getWindowHandles().contains(googleWH));
         assertTrue(d.getWindowHandles().contains(bingWH));
     }
 
-    @Test
+    @Test(expected = Exception.class)
     public void switchBetween3WindowsThenDeleteFirstOne() {
         WebDriver d = getDriver();
 
@@ -77,20 +71,16 @@ public class WindowSwitchingTest extends BaseTest {
         assertTrue(!bingWH.equals(googleWH));
         assertTrue(!bingWH.equals(yahooWH));
 
+        // Switch to google window and close it
         d.switchTo().window(googleWH);
-        // Delete yahoo window and try to access it :should cause an error
-        boolean threw = false;
-        try {
-            d.close();
-            d.getTitle();
-        } catch (Throwable t) {
-            threw = true;
-        }
-        assertTrue(threw);
+        d.close();
 
-        // Swtich to google window and notice that it's the only left (bing was closed as it was a child of yahoo)
+        // Notice that yahoo and bing are the only left
         assertEquals(d.getWindowHandles().size(), 2);
         assertTrue(d.getWindowHandles().contains(yahooWH));
         assertTrue(d.getWindowHandles().contains(bingWH));
+
+        // Try getting the title of the, now closed, google window and cause an Exception
+        d.getTitle();
     }
 }
