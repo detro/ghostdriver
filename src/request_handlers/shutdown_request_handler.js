d505eb49668edb17c3983adae0456f1d3a304a83
Jim Evans <james.h.evans.jr@gmail.com>
2012-09-06 14:01:08 -0400

    Adding myself to the copyright notice.

--- src/request_handlers/shutdown_request_handler.js
+++ src/request_handlers/shutdown_request_handler.js
@@ -2,6 +2,7 @@
 This file is part of the GhostDriver project from Neustar inc.
 
 Copyright (c) 2012, Ivan De Marino <ivan.de.marino@gmail.com> - Neustar inc.
+Copyright (c) 2010, Jim Evans <james.h.evans.jr@gmail.com> - Salesforce.com
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification,
