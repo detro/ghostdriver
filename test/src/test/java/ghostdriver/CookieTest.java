694c9d8e81ece50d94a4fdc6abc1487bd48e39af
Ivan De Marino <ivan.de.marino@gmail.com>
2012-09-12 20:10:09 +0100

    Expanded CookieTest to more intensively stress the Cookie API.
    This test covers latest fixed scenarios that can be found in PhantomJS "ghostdriver-dev" branch (by detro)

--- test/src/test/java/ghostdriver/CookieTest.java
+++ test/src/test/java/ghostdriver/CookieTest.java
@@ -3,20 +3,28 @@ package ghostdriver;
 import org.junit.Test;
 import java.util.Set;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.Cookie;
 
 public class CookieTest extends BaseTest {
     @Test
-    public void shouldBeAbleToAddCookie() {
+    public void shouldBeAbleToAddCookie() throws InterruptedException {
         WebDriver d = getDriver();
-        d.get("http://docs.wpm.neustar.biz/testscript-api/index.html");
+        d.get("http://www.github.com");
 
+        // Clear all cookies
+        assertTrue(d.manage().getCookies().size() > 0);
         d.manage().deleteAllCookies();
+        assertEquals(d.manage().getCookies().size(), 0);
+
+
         Cookie myCookie = new Cookie("foo", "bar");
         d.manage().addCookie(myCookie);
+        assertEquals(d.manage().getCookies().size(), 1);
 
-        Set<Cookie> allCookies = d.manage().getCookies();
-        assertEquals(1, allCookies.size());
+        Cookie myCookieIsBack = d.manage().getCookieNamed("foo");
+        assertTrue(myCookieIsBack.getDomain().contains("github"));
     }
 }
