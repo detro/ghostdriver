54e372d0917fb7f44c76fe275589c0933ba97dc8
Jim Evans <james.h.evans.jr@gmail.com>
2012-09-13 14:12:20 -0400

    Adding additional failing cookie expiration test.

--- test/src/test/java/ghostdriver/CookieTest.java
+++ test/src/test/java/ghostdriver/CookieTest.java
@@ -1,9 +1,11 @@
 package ghostdriver;
 
 import org.junit.Test;
+import java.util.Date;
 import java.util.Set;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.Cookie;
@@ -26,5 +28,26 @@ public class CookieTest extends BaseTest {
 
         Cookie myCookieIsBack = d.manage().getCookieNamed("foo");
         assertTrue(myCookieIsBack.getDomain().contains("github"));
+    }
+
+	@Test
+    public void shouldRetainCookieExpiry() {
+        WebDriver d = getDriver();
+        d.get("http://www.github.com");
+
+        // Clear all cookies
+        assertTrue(d.manage().getCookies().size() > 0);
+        d.manage().deleteAllCookies();
+        assertEquals(d.manage().getCookies().size(), 0);
+
+        Cookie addedCookie =
+            new Cookie.Builder("fish", "cod")
+                .expiresOn(new Date(System.currentTimeMillis() + 100000))
+                .build();
+        d.manage().addCookie(addedCookie);
+
+        Cookie retrieved = d.manage().getCookieNamed("fish");
+        assertNotNull(retrieved);
+        assertEquals(addedCookie.getExpiry(), retrieved.getExpiry());
     }
 }
