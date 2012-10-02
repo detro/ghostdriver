48168edacbb1a66df507719d816b240fba84bc88
Ivan De Marino <ivan.de.marino@gmail.com>
2012-10-02 22:40:09 +0100

    Fixing typo.

--- src/session.js
+++ src/session.js
@@ -35,7 +35,7 @@ ghostdriver.Session = function(desiredCapabilities) {
         "version" : phantom.version.major + '.' + phantom.version.minor + '.' + phantom.version.patch,
         "platform" : ghostdriver.system.os.name + '-' + ghostdriver.system.os.version + '-' + ghostdriver.system.os.architecture,
         "javascriptEnabled" : true,
-        "takesScreenshot" : "VERO",
+        "takesScreenshot" : true,
         "handlesAlerts" : false,            //< TODO
         "databaseEnabled" : false,          //< TODO
         "locationContextEnabled" : false,   //< TODO Target is 1.1
