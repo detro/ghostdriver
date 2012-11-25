18431415464943c9fc40dda5b1e2dcedf6d3cda5
Ivan De Marino <ivan.de.marino@gmail.com>
2012-11-25 18:23:29 +0000

    Java Binding 1.0.0 ready.
    The most important change is that the property/capability `phantomjs.ghostdriver.path` is now optional.
    If not used, the Binding will use PhantomJS internal GhostDriver.

--- binding/java/src/main/java/org/openqa/selenium/phantomjs/PhantomJSCommandExecutor.java
+++ binding/java/src/main/java/org/openqa/selenium/phantomjs/PhantomJSCommandExecutor.java
@@ -1,8 +1,33 @@
+/*
+This file is part of the GhostDriver by Ivan De Marino <http://ivandemarino.me>.
+
+Copyright (c) 2012, Ivan De Marino <http://ivandemarino.me>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 package org.openqa.selenium.phantomjs;
 
 import com.google.common.base.Throwables;
-
-import org.openqa.selenium.Beta;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.remote.Command;
 import org.openqa.selenium.remote.DriverCommand;
@@ -13,62 +38,62 @@ import java.io.IOException;
 import java.net.ConnectException;
 
 /**
- * A specialized {@link org.openqa.selenium.remote.HttpCommandExecutor} that will use a {@link
- * PhantomJSDriverService} that lives and dies with a single WebDriver session.
+ * A specialized {@link org.openqa.selenium.remote.HttpCommandExecutor} that will use a
+ * {@link PhantomJSDriverService} that lives and dies with a single WebDriver session.
+ * <p/>
+ * The service will be restarted upon each new session request and shutdown after each quit command.
+ * <p/>
+ * NOTE: Yes, the design of this class is heavily inspired by
+ * {@link org.openqa.selenium.chrome.ChromeCommandExecutor}.
  *
- * The service will be restarted upon each new session request and shutdown after each quit
- * command.
- *
- * NOTE: Yes, the design of this class is heavily inspired by {@link
- * org.openqa.selenium.chrome.ChromeCommandExecutor}.
+ * @author Ivan De Marino <http://ivandemarino.me>
  */
-@Beta
 class PhantomJSCommandExecutor extends HttpCommandExecutor {
 
-  private final PhantomJSDriverService service;
+    private final PhantomJSDriverService service;
 
-  /**
-   * Creates a new PhantomJSCommandExecutor. The PhantomJSCommandExecutor will communicate with
-   * the PhantomJS/GhostDriver through the given {@code service}.
-   *
-   * @param service The PhantomJSDriverService to send commands to.
-   */
-  public PhantomJSCommandExecutor(PhantomJSDriverService service) {
-    super(service.getUrl());
-    this.service = service;
-  }
-
-  /**
-   * Sends the {@code command} to the PhantomJS/GhostDriver server for execution. The server will
-   * be started if requesting a new session. Likewise, if terminating a session, the server will
-   * be shutdown once a response is received.
-   *
-   * @param command The command to execute.
-   * @return The command response.
-   * @throws java.io.IOException If an I/O error occurs while sending the command.
-   */
-  @Override
-  public Response execute(Command command) throws IOException {
-    if (DriverCommand.NEW_SESSION.equals(command.getName())) {
-      service.start();
+    /**
+     * Creates a new PhantomJSCommandExecutor.
+     * The PhantomJSCommandExecutor will communicate with the PhantomJS/GhostDriver through the given {@code service}.
+     *
+     * @param service The PhantomJSDriverService to send commands to.
+     */
+    public PhantomJSCommandExecutor(PhantomJSDriverService service) {
+        super(service.getUrl());
+        this.service = service;
     }
 
-    try {
-      return super.execute(command);
-    } catch (Throwable t) {
-      Throwable rootCause = Throwables.getRootCause(t);
-      if (rootCause instanceof ConnectException &&
-          "Connection refused".equals(rootCause.getMessage()) &&
-          !service.isRunning()) {
-        throw new WebDriverException("The PhantomJS/GhostDriver server has unexpectedly died!", t);
-      }
-      Throwables.propagateIfPossible(t);
-      throw new WebDriverException(t);
-    } finally {
-      if (DriverCommand.QUIT.equals(command.getName())) {
-        service.stop();
-      }
+    /**
+     * Sends the {@code command} to the PhantomJS/GhostDriver server for execution.
+     * The server will be started if requesting a new session.
+     * Likewise, if terminating a session, the server will be shutdown once a response is received.
+     *
+     * @param command The command to execute.
+     * @return The command response.
+     * @throws java.io.IOException If an I/O error occurs while sending the command.
+     */
+    @Override
+    public Response execute(Command command) throws IOException {
+        if (DriverCommand.NEW_SESSION.equals(command.getName())) {
+            service.start();
+        }
+
+        try {
+            return super.execute(command);
+        } catch (Throwable t) {
+            Throwable rootCause = Throwables.getRootCause(t);
+            if (rootCause instanceof ConnectException &&
+                    "Connection refused".equals(rootCause.getMessage()) &&
+                    !service.isRunning()) {
+                throw new WebDriverException("The PhantomJS/GhostDriver server has unexpectedly died!", t);
+            }
+            Throwables.propagateIfPossible(t);
+            throw new WebDriverException(t);
+        } finally {
+            if (DriverCommand.QUIT.equals(command.getName())) {
+                service.stop();
+            }
+        }
     }
-  }
 }
 
--- binding/java/src/main/java/org/openqa/selenium/phantomjs/PhantomJSDriver.java
+++ binding/java/src/main/java/org/openqa/selenium/phantomjs/PhantomJSDriver.java
@@ -1,106 +1,111 @@
 /*
-Copyright 2012 Ivan De Marino <ivan.de.marino@gmail.com>
-Copyright 2012 Selenium committers
-Copyright 2012 Software Freedom Conservancy
+This file is part of the GhostDriver by Ivan De Marino <http://ivandemarino.me>.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+Copyright (c) 2012, Ivan De Marino <http://ivandemarino.me>
+All rights reserved.
 
-     http://www.apache.org/licenses/LICENSE-2.0
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+    * Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 package org.openqa.selenium.phantomjs;
 
-import org.openqa.selenium.*;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.remote.DriverCommand;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.remote.service.DriverCommandExecutor;
 
 /**
- * @class PhantomJSDriver
- *
- * A {@link org.openqa.selenium.WebDriver} implementation that controls a PhantomJS running locally
- * GhostDriver. This class is provided as a convenience for easily testing PhantomJS. The control
- * server which each instance communicates with will live and die with the instance.
- *
+ * A {@link org.openqa.selenium.WebDriver} implementation that controls a PhantomJS running in Remote WebDriver mode.
+ * This class is provided as a convenience for easily testing PhantomJS.
+ * The control server which each instance communicates with will live and die with the instance.
+ * <p/>
+ * The Driver requires to optionally set some Capabilities or Environment Variables:
+ * <ul>
+ * <li>{@link PhantomJSDriverService#PHANTOMJS_EXECUTABLE_PATH_PROPERTY}</li>
+ * <li>{@link PhantomJSDriverService#PHANTOMJS_GHOSTDRIVER_PATH_PROPERTY}</li>
+ * </ul>
+ * <p/>
+ * {@link PhantomJSDriverService#PHANTOMJS_EXECUTABLE_PATH_PROPERTY} is required only if the executable
+ * <code>phantomjs</code> is not available through the <code>$PATH</code> environment variable:
+ * you can provide it either via the {@link Capabilities} construction parameter object,
+ * or via {@link System} Property.
+ * <p/>
+ * {@link PhantomJSDriverService#PHANTOMJS_GHOSTDRIVER_PATH_PROPERTY} is optional in case you want to use a specific
+ * version of GhostDriver (i.e. during development of GhostDriver).
+ * You can provide it either via the {@link Capabilities} construction parameter object,
+ * or via {@link System} Property.
+ * <p/>
  * Instead, if you have a PhantomJS WebDriver process already running, you can instead use {@link
  * RemoteWebDriver#RemoteWebDriver(java.net.URL, org.openqa.selenium.Capabilities)} to delegate the
  * execution of your WebDriver/Selenium scripts to it. Of course, in that case you will than be in
  * charge to control the life-cycle of the PhantomJS process.
+ * <p/>
+ * NOTE: PhantomJS Remote WebDriver mode is implemented via
+ * <a href="https://github.com/detro/ghostdriver">GhostDriver</a>.
+ * It's a separate project that, at every stable release, is merged into PhantomJS.
+ * If interested in developing (contributing to) GhostDriver, it's possible to run PhantomJS and pass GhostDriver as
+ * a script.
+ * <p/>
+ * NOTE: The design of this class is heavily inspired by {@link org.openqa.selenium.chrome.ChromeDriver}.
  *
- * NOTE: <a href="https://github.com/detro/ghostdriver">GhostDriver</a> it's the PhantomJS Driver.
- * It's currently a separate project but will later on become one with PhantomJS, making the
- * requirement of the dedicated capability entry obsolete. Also, GhostDriver, being it a work in
- * progress, might rely on a specific version of PhantomJS: please read carefully the instructions
- * available at the project homepage.
- *
- * NOTE: Yes, the design of this class is heavily inspired by {@link
- * org.openqa.selenium.chrome.ChromeDriver}.
+ * @author Ivan De Marino <http://ivandemarino.me>
  * @see PhantomJSDriverService#createDefaultService()
  */
-@Beta
 public class PhantomJSDriver extends RemoteWebDriver implements TakesScreenshot {
 
-//  /**
-//   * Creates a new PhantomJSDriver instance.
-//   *
-//   * @see org.openqa.selenium.phantomjs.PhantomJSDriverService#createDefaultService() for
-//   *      configuration details.
-//   */
-//  public PhantomJSDriver() {
-//    this(PhantomJSDriverService.createDefaultService(), DesiredCapabilities.phantomjs());
-//  }
-//
-//  /**
-//   * Creates a new PhantomJSDriver instance, using a specific PhantomJSDriverService.
-//   *
-//   * @param service The PhantomJSDriverService to use.
-//   */
-//  public PhantomJSDriver(PhantomJSDriverService service) {
-//    this(service, DesiredCapabilities.phantomjs());
-//  }
+    /**
+     * Creates a new PhantomJSDriver instance.
+     *
+     * @param desiredCapabilities The capabilities required from PhantomJS/GhostDriver.
+     * @see org.openqa.selenium.phantomjs.PhantomJSDriverService#createDefaultService() for configuration details.
+     */
+    public PhantomJSDriver(Capabilities desiredCapabilities) {
+        this(PhantomJSDriverService.createDefaultService(desiredCapabilities), desiredCapabilities);
+    }
 
-  /**
-   * Creates a new PhantomJSDriver instance.
-   *
-   * @param desiredCapabilities The capabilities required from PhantomJS/GhostDriver.
-   * @see org.openqa.selenium.phantomjs.PhantomJSDriverService#createDefaultService() for
-   *      configuration details.
-   */
-  public PhantomJSDriver(Capabilities desiredCapabilities) {
-    this(PhantomJSDriverService.createDefaultService(desiredCapabilities), desiredCapabilities);
-  }
+    /**
+     * Creates a new PhantomJSDriver instance. The {@code service} will be started along with the
+     * driver, and shutdown upon calling {@link #quit()}.
+     *
+     * @param service             The service to use.
+     * @param desiredCapabilities The capabilities required from PhantomJS/GhostDriver.
+     */
+    public PhantomJSDriver(PhantomJSDriverService service, Capabilities desiredCapabilities) {
+        super(new DriverCommandExecutor(service), desiredCapabilities);
+    }
 
-  /**
-   * Creates a new PhantomJSDriver instance. The {@code service} will be started along with the
-   * driver, and shutdown upon calling {@link #quit()}.
-   *
-   * @param service             The service to use.
-   * @param desiredCapabilities The capabilities required from PhantomJS/GhostDriver.
-   */
-  public PhantomJSDriver(PhantomJSDriverService service, Capabilities desiredCapabilities) {
-    super(new DriverCommandExecutor(service), desiredCapabilities);
-  }
-
-  /**
-   * Take screenshot of the current window.
-   *
-   * @param target The target type/format of the Screenshot
-   * @return Screenshot of current window, in the requested format
-   * @see TakesScreenshot#getScreenshotAs(org.openqa.selenium.OutputType)
-   */
-  @Override
-  public <X> X getScreenshotAs(OutputType<X> target) throws WebDriverException {
-    // Get the screenshot as base64 and convert it to the requested type (i.e. OutputType<T>)
-    String base64 = (String) execute(DriverCommand.SCREENSHOT).getValue();
-    return target.convertFromBase64Png(base64);
-  }
+    /**
+     * Take screenshot of the current window.
+     *
+     * @param target The target type/format of the Screenshot
+     * @return Screenshot of current window, in the requested format
+     * @see TakesScreenshot#getScreenshotAs(org.openqa.selenium.OutputType)
+     */
+    @Override
+    public <X> X getScreenshotAs(OutputType<X> target) throws WebDriverException {
+        // Get the screenshot as base64 and convert it to the requested type (i.e. OutputType<T>)
+        String base64 = (String) execute(DriverCommand.SCREENSHOT).getValue();
+        return target.convertFromBase64Png(base64);
+    }
 }
