d737f593adf040ac69e6d977b24d695d691505fb
Ivan De Marino <ivan.de.marino@gmail.com>
2013-02-12 19:02:53 +0000

    Very basic stress tests.

--- test/java/src/test/java/ghostdriver/DriverBasicTest.java
+++ test/java/src/test/java/ghostdriver/DriverBasicTest.java
@@ -1,6 +1,8 @@
 package ghostdriver;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 
 public class DriverBasicTest extends BaseTest {
@@ -11,5 +13,27 @@ public class DriverBasicTest extends BaseTest {
 
         d.get("http://www.google.com");
         d.quit();
+    }
+
+    @Test
+    public void shouldSurviveExecutingManyTimesTheSameCommand() {
+        WebDriver d = getDriver();
+        d.get("http://www.google.com");
+        for (int j = 0; j < 100; j++) {
+            try {
+                d.findElement(By.linkText(org.apache.commons.lang3.RandomStringUtils.randomAlphabetic(4))).isDisplayed();
+            } catch (NoSuchElementException nsee) {
+                // swallow exceptions: we don't care about the result
+            }
+        }
+    }
+
+    @Test
+    public void shouldSurviveExecutingManyTimesTheSameTest() throws Exception {
+        for (int i = 0; i < 100; ++i) {
+            prepareDriver();
+            shouldSurviveExecutingManyTimesTheSameCommand();
+            quitDriver();
+        }
     }
 }
