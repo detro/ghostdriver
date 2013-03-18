71b347da74002392b5c2068fd702d0e86aaaf236
Ivan De Marino <ivan.de.marino@gmail.com>
2013-03-18 01:24:30 +0000

    Bumping to version 1.0.3.

--- binding/java/README.md
+++ binding/java/README.md
@@ -5,3 +5,8 @@
 $ JAVA_HOME=/path/to/java/1.6.x/home ./gradlew jars
 ```
 Selenium requires JAVA 6.
+
+# Uploading Jars to Sonatype OSS
+
+1. ./gradlew uploadArchives
+2. Continue publishing of archives at https://oss.sonatype.org/index.html#view-repositories
--- src/main.js
+++ src/main.js
@@ -32,7 +32,7 @@ var ghostdriver = {
         hub     : require("./hub_register.js"),
         logger  : require("./logger.js"),
         config  : null,                         //< will be set in a short while
-        version : "1.0.3-dev"
+        version : "1.0.3"
     },
     server = require("webserver").create(),
     router,
